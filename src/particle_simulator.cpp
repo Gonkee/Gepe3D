dbb1562c743288f834f6b31a3a029d65ac8af0f7
Gonkee <41216697+Gonkee@users.noreply.github.com>
2026-09-06 17:17:39 +1000

    switched to opencl cpp headers and made a start on the boilerplate

--- src/particle_simulator.cpp
+++ src/particle_simulator.cpp
@@ -1,1 +1,17 @@
 #include "particle_simulator.hpp"
+#include "kernels.cl.h"
+#include <vector>
+#include <stdexcept>
+
+void setupOpenCL() {
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    if (platforms.empty()) throw std::runtime_error("Could not find OpenCL platform");
+
+    cl::Platform platform = platforms[0];
+    if (cl::Platform::setDefault(platform) != platform)
+        throw std::runtime_error("Error setting default platform");
+
+    std::string source(reinterpret_cast<const char*>(kernels_cl), sizeof(kernels_cl));
+    cl::Program kernels(source);
+}
